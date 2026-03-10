eade2ee0d11d30ff5769a5bd2eb50ff322c1e0fb
Jayant Kumar Jha <166705981+JayantKumarJha@users.noreply.github.com>
2026-03-10 15:17:56 +0530

    Refactor HireMe component layout and contact info
    Updated the HireMe component with new dimensions, optimized text formatting, and changed the email link.

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const HireMe = () => {
   return (
     <div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute">
-      <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
+      <div className="w-36 h-auto flex items-center justify-center relative md:w-24">
         <svg
           className="fill-dark animate-spin-slow dark:fill-light"
           viewBox="0 0 100 100"
@@ -17,12 +17,12 @@ const HireMe = () => {
           />
           <text>
             <textPath xlinkHref="#circlePath" className="text-[10px] font-semibold tracking-widest">
-              Optimization Engineer • AI/ML Consultant •&nbsp;
+              Optimization Engineer•AI/ML Consultant •&nbsp;
             </textPath>
           </text>
         </svg>
         <Link
-          href="mailto:jayant@example.com"
+          href="mailto:iamjayant.ieor23@gmail.com"
           className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark transition-all duration-300 dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:w-12 md:h-12 md:text-[10px]"
         >
           Hire Me
